94f103ed6feee9ae9b6a2ff89893766ee50f93af
Сергей Цуккер <Mart1612@yandex.ru>
2023-04-05 21:48:36 +0300

    final deploy

--- infra_project/infra_app/views.py
+++ infra_project/infra_app/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse('У меня получилось!')
+    return HttpResponse('У меня получилось! Отличная работа!')
 
 
 def second_page(request):
